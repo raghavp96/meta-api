7de635cbcba58e9fc7c7822755f398bc95fdd3f4
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-08-02 09:19:55 -0400

    using port from environment variable

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,16 @@
 from app import application
+import os
 
 if __name__ == "__main__":
-    application.run()
+    # ON_HEROKU will be 1 if True
+    ON_HEROKU = os.environ.get('ON_HEROKU', None)
+
+    if ON_HEROKU == 1:
+        print("Running on Heroku...")
+        port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+        
+    else:
+        print("Running on local...")
+        port = 8000
+        
+    application.run(host='0.0.0.0', port=port)
